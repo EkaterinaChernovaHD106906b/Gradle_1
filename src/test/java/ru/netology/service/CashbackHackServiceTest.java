93b51b95001981f919dd29ba7b686de0bdecd961
Екатерина Чернова <hd106906bqwerty@gmail.com>
2022-08-04 14:36:40 +0300

    Test was added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -35,6 +35,16 @@ public class CashbackHackServiceTest {
         int expected = 999;
         assertEquals(actual, expected);
     }
+    @Test
+    void myTest4() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
 
 }
 
